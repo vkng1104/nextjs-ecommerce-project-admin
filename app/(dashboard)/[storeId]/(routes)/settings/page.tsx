5e16009d516f96e7c7f8fa7a3021d43e7707467a
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-02 20:58:02 +0700

    create setting page

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prisma_db from "@/lib/prismadb";
+import { SettingsForm } from "./components/settings-form";
+
+interface SettingPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingPage: React.FC<SettingPageProps> = async ({ params }) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prisma_db.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
+};
+
+export default SettingPage;
