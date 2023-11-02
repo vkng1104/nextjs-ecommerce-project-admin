be4d28f3df357cb949d63b5a70b2c1e55127b5f0
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-02 21:01:45 +0700

    Update settings-form.tsx
    - Fix hydration error

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -25,6 +25,8 @@ import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 
+import { useOrigin } from "@/hooks/use-origin";
+
 interface SettingsFormProps {
   initialData: Store;
 }
@@ -39,6 +41,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
