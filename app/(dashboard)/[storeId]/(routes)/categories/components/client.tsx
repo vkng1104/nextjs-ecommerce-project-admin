e30ed68675366f5c4bc7841d225f7635a02ccc16
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-03 22:14:08 +0700

    category search bar - fix not working

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -33,7 +33,7 @@ export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
+      <DataTable searchKey="name" columns={columns} data={data} />
       <Heading title="API" description="API calls for categories" />
       <Separator />
       <ApiList entityName="categories" entityIdName="categoryId" />
